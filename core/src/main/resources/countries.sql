54743d4af058cd50335a63db8468eaf36094ebbd
hgilmore <hgilmore@underarmour.com>
2014-11-03 13:54:29 -0500

    changed countries.sql was out of sync with the working local schema

--- core/src/main/resources/countries.sql
+++ core/src/main/resources/countries.sql
@@ -3,19 +3,19 @@ DROP TABLE countries;
 
 CREATE TABLE countries
 (
-  common_name character varying(32) NOT NULL,
-  formal_name character varying(128) NOT NULL,
-  _type character varying(32) NOT NULL,
-  sub_type character varying(32),
-  sovereignty character varying(32),
-  capital character varying(128) NOT NULL,
-  iso_4217_currency_code character varying(3) NOT NULL,
-  iso_4217_currency_name character varying(16) NOT NULL,
-  itu_t_telephone_code integer NOT NULL,
-  iso_3166_1_2_letter_code character(2) NOT NULL,
-  iso_3166_1_3_letter_code character(3) NOT NULL,
-  iso_3166_1_number integer NOT NULL,
-  iana_country_code_tld character(3) NOT NULL
+  common_name character varying(128) NOT NULL,
+  formal_name character varying(128),
+  _type character varying(128) NOT NULL,
+  sub_type character varying(128),
+  sovereignty character varying(128),
+  capital character varying(128),
+  iso_4217_currency_code character varying(16),
+  iso_4217_currency_name character varying(16),
+  itu_t_telephone_code character varying(32),
+  iso_3166_1_2_letter_code character(2),
+  iso_3166_1_3_letter_code character(3),
+  iso_3166_1_number integer,
+  iana_country_code_tld character varying(16)
 )
 WITH (
   OIDS=FALSE
